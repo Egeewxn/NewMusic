995602a55916c5c759d283c91af1fd579619032a
Egeewxn <lexydc2@gmail.com>
2026-04-26 13:56:21 +0300

    Update callsmusic.py

--- callsmusic/callsmusic.py
+++ callsmusic/callsmusic.py
@@ -2,11 +2,12 @@ from pyrogram import Client
 from pytgcalls import GroupCallFactory
 from config import API_ID, API_HASH, BOT_TOKEN, STRING_SESSION
 
-# Ana Bot Kontrolü
+# Ana Bot
 app = Client("JazeBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 # Asistan (String Session)
 asistan = Client("JazeAsistan", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION)
 
-# Ses Motoru
+# SES MOTORU GÜNCELLEMESİ
+# 1.0.0 sürümü için en stabil başlangıç yolu budur:
 pytgcalls = GroupCallFactory(asistan).get_group_call()
